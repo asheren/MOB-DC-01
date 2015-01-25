67a3df894123fbec32bf2c25cc0cf153f432b6dd
Allison McMillan <allisonm@generalassemb.ly>
2015-01-25 16:51:51 -0500

    WIP assessment 3

--- Homework/Week 3/Assessment - BlackJack/Assessment 3/CardGame.swift
+++ Homework/Week 3/Assessment - BlackJack/Assessment 3/CardGame.swift
@@ -8,10 +8,25 @@
 
 import Foundation
 protocol BlackJack {
+    var deal: Bool { get set }
     // Require a deal method
+    
+    func firstHand -> Int
     // Require a first hand method
 }
 
 class CardGame: BlackJack {
+    var deal: Bool = true
+  
+   var myPlayer = Player()
+    var total = 0
+    func firstHand -> Int {
+        let rand1 = Int(arc4random(12))
+        let rand2 = Int(arc4random(12))
+        return rand1 + rand2
+    }
     
+    func CPU -> Int {
+       let cpu = Int(arc4random(12...21))
+    }
 }
--- Homework/Week 3/Assessment - BlackJack/Assessment 3/ViewController.swift
+++ Homework/Week 3/Assessment - BlackJack/Assessment 3/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     // ** Set up Player class and CardGame class in their respective provided files. Follow the protocol rules for the CardGame class.
     // When card game first starts, a new player is generated and handed out 2 cards (between 1 and 11). Hint: generate 2 random numbers and add them together
     // A value for the CPU is also generated between 12 and 21 (random Int)
+    
+    
     // When the green view is double tapped (tap gesture recognizer), generate a new card (between 1 and 11) for the player, and add it to their score
     // If the total adds up to over 21, change the label text to "Game over, you lose!" and remove the ability for the player to keep tapping for cards
     // If the player wishes to not get dealt any more cards, the player can swipe right to end game (swipe gesture recognizer)
