64fdc19bea703db2f8ab04447095d538f73c2508
Allison McMillan <allisonm@generalassemb.ly>
2015-02-19 10:22:58 -0500

    first part of dev workshop

---
+++ Class Work/workshop/workshop test/workshop test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  workshop test
+//
+//  Created by Allison Sheren McMillan on 2/18/15.
+//  Copyright (c) 2015 Allison McMillan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
