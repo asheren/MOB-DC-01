67bc6623b3ef38f21d450e6ceb3536e2bef80fd8
Allison McMillan <allisonm@generalassemb.ly>
2015-03-02 19:35:24 -0500

    workshop files

---
+++ Class Work/workshop/workshop/workshop/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  workshop
+//
+//  Created by Allison Sheren McMillan on 2/26/15.
+//  Copyright (c) 2015 Allison McMillan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var printNameButton: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
